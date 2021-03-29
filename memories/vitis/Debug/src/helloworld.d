2fa184087a9f6e48662fa626d9863b631a621d67
provoostkris <provoostkris@hotmail.com>
2021-03-29 22:07:23 +0200

    Creation of the vitis project

---
+++ memories/vitis/Debug/src/helloworld.d
@@ -0,0 +1,36 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_types.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/bspconfig.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_io.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xpseudo_asm.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xreg_cortexa9.h \
+ C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/bspconfig.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_io.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xpseudo_asm.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xreg_cortexa9.h:
+
+C:/Users/Kris/workspace/soc_wrapper_2/export/soc_wrapper_2/sw/soc_wrapper_2/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h:
